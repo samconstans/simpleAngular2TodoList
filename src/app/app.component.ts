bfc661cf0cf5caa1842d9dd9a17b04cef9c946d3
Anastasiya Vovk <nastia-vovk@ukr.net>
2017-04-19 13:47:47 +0300

    added Todo list with Angular2

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: []
+})
+export class AppComponent {
+
+  newTodo: Todo = new Todo();
+
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  onAddTodo(todo) {
+    this.todoDataService.addTodo(todo);
+  }
+
+  // Service is now available as this.todoDataService
+  onToggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  onRemoveTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+  return this.todoDataService.getAllTodos();
+  }
+
+}
